30855eb81a27b72d1f031f0b1dc93fa6c96eedf5
ThiagoDRangel <info.tec.campos@gmail.com>
2023-03-25 21:08:39 -0300

    Configurando os componentes recipes

---
+++ src/components/Recipes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardRecipe from './CardRecipe';
+
+function Recipes() {
+  return (
+    <main>
+      <CardRecipe />
+    </main>
+  );
+}
+
+export default Recipes;

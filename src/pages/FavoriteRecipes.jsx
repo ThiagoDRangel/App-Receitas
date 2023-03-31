fae66e9e0b35b31429b3bff25e547094e3cb07db
ThiagoDRangel <info.tec.campos@gmail.com>
2023-03-31 14:04:49 -0300

    Configurando página FavoriteRecipes

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -4,9 +4,45 @@ import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 
 function FavoriteRecipes() {
+  const [urlCopied, setUrlCopied] = useState(false);
+  
+  const onClickShare = () => {
+    const URL = window.location.href;
+    const TIMEOUT = 2000;
+    copy(URL.replace('/favorite-recipes', ''));
+    setUrlCopied(true);
+
+    setTimeout(() => setUrlCopied(false), TIMEOUT);
+  };
+
   return (
     <main>
-
+      <Header title=" Favorite Recipes" />
+      <button
+        type="button"
+      >
+        All
+      </button>
+      <button
+        type="button"
+      >
+        Meals
+      </button>
+      <button
+        type="button"
+      >
+        Drinks
+      </button>
+      <button
+        onClick={ () => onClickShare() }
+      >
+        <img
+          alt="share Icon"
+          className="icons"
+          src={ shareIcon}
+        />
+      </button>
+      {urlCopied && <p>Link copied!</p>}
     </main>
   );
 }
